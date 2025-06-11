29bbcf3c10232cbdb06458296227e188478a69b7
Kyle-the-coder <kylethacoder@gmail.com>
2025-06-11 16:58:18 -0600

    updated

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,24 @@
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
+// import { Nav } from "../components/Nav/Nav";
+// import { Footer } from "../components/Footer/Footer";
+// import { Loader } from "../components/Loader/Loader";
+
+export function MainLayout() {
+  const { state } = useNavigation();
+
+  return (
+    <div className="main-container">
+      {/* <Nav /> */}
+      {state === "loading" ? (
+        {
+          /* <Loader /> */
+        }
+      ) : (
+        <div>
+          <Outlet />
+        </div>
+      )}
+      {/* <Footer /> */}
+    </div>
+  );
+}
