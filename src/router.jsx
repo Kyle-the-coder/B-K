c867eae342d25131686a4f036455c8a3edec780e
Kyle-the-coder <kylethacoder@gmail.com>
2025-06-11 16:59:45 -0600

    updated

---
+++ src/router.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+import { MainLayout } from "./layouts/MainLayout";
+import { ErrorLayout } from "./layouts/ErrorLayout";
+// import { landingRoute } from "./pages/Landing/Landing";
+// import { aboutRoute } from "./pages/About/About";
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <ErrorLayout />,
+    children: [
+      //   { path: "/", ...landingRoute },
+      //   { path: "/about", ...aboutRoute },
+    ],
+  },
+]);
